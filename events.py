fa67c5ae84241915d18c45499e5a7ac868955073
Rafael Dua <rafa.dua0807@gmail.com>
2025-12-06 18:24:09 -0500

    create events.py outline

---
+++ events.py
@@ -0,0 +1,14 @@
+class MarketEvent:
+    def __init__(self, name, date, ohlcv_impact):
+        self.name = name
+        self.date = date
+        self.ohlcv_impact = ohlcv_impact
+
+    def __repr__(self):
+        return f"MarketEvent(name={self.name}, date={self.date}, ohlcv_impact={self.ohlcv_impact})"
+    
+class SignalEvent:
+
+
+class OrderEvent:
+
